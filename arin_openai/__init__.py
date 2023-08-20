bb212c9f9bd49e05dc19450e90794a7da175fea7
JaapOosterbroek <jaap@secureailabs.com>
2023-08-20 08:24:01 +0300

    version bump

--- arin_openai/__init__.py
+++ arin_openai/__init__.py
@@ -2,4 +2,4 @@ __title__ = "arin-openai"
 __author__ = "jaap@arrayinsights.com"
 __license__ = ""
 __copyright__ = "Copyright 2023-present Array Insights"
-__version__ = "0.2.0"  # https://peps.python.org/pep-0440/
+__version__ = "0.4.0"  # https://peps.python.org/pep-0440/
